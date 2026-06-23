fe99e7a7874cd68b3dae7b71f23ca6d3d4f16161
Daniel Kajuga <kajugadaniels@gmail.com>
2026-06-23 12:23:33 +0200

    feat(database): export shared prisma client

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { normalizeDatabaseUrl } from './database-url.util';
+export {
+  createPrismaClient,
+  type CreatePrismaClientOptions,
+} from './prisma-client.factory';
+export * from '../src/generated/prisma/client';
